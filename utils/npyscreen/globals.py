4839bff454b5667cd6dd8c0a9db84bbb01786032
Yellow Heart <Ayrtonkyle@gmail.com>
2023-09-08 13:02:03 -0700

    Delete utils/npyscreen directory
    moving to main dir

--- utils/npyscreen/globals.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-DEBUG = False
-DISABLE_RESIZE_SYSTEM = False
--- utils/npyscreen/npysGlobalOptions.py
+++
@@ -1,2 +0,0 @@
-DISABLE_ALL_COLORS = False
-ASCII_ONLY         = False # See the safe_string function in wgwidget.  At the moment the encoding is not safe
